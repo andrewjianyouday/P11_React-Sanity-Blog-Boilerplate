cd9c444fd00398af43dd4914baa1489e026e8ed0
andrewjianyouday <andrewjianyouday@gmail.com>
2022-02-20 23:19:00 +0100

    setup reactApp and sanity project

---
+++ src/client.js
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client'
+
+// Run @sanity/client
+// To allow React to jump to find the Sanity Client.js 
+// and use the project id to pull the studio and make it a sanity project.
+
+export default sanityClient({
+    projectId: "uh9kq253",
+    detaset:"production"
+})
