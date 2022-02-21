2945777d3eb986044f6ff9c7601461a30dd0b68a
andrewjianyouday <andrewjianyouday@gmail.com>
2022-02-21 21:52:10 +0100

    setup react router

---
+++ src/components/AllPosts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AllPosts() {
+    console.log('why not print out')
+    return <h1> hello hello </h1>
+    
+}
---
+++ src/components/OnePost.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function OnePost(){
+    return <h2> THIS PAGE HAS One Post </h2>
+}
